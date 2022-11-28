c244050d414ca05c561a01cf578bd26348b6d835
DSnoad <danialsnoad@hotmail.com>
2022-11-28 18:48:03 +0000

    Initial skeleton put in place

--- run.py
+++ run.py
@@ -1,1 +1,26 @@
+def new_game():
+    """
+    The start of the Game, Introducing to the player
+    """
+    
+
+
+def check_answers():
+    """
+    A Function that will check the correct answers for the User
+    """
+    
+
+
+def display_score():
+    """
+    A score card for the player to keep track with
+    """
+    
+
+
+def play_again():
+    """
+    An option to restart the quiz
+    """
 
