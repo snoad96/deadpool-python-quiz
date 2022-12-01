c973e9255c1253d850ea6c3b8137f42e31c33981
DSnoad <danialsnoad@hotmail.com>
2022-12-01 13:55:10 +0000

    Fix errors and remove trailing whitespace

--- run.py
+++ run.py
@@ -3,13 +3,11 @@ name = ""
 
 def game_intro():
     """
-    TheIntro to the game, Introducing to the player 
+    TheIntro to the game, Introducing to the player
     and asks for name
     """
     global name
-    name = input("Unless your name is Deadpool.."
-        "Enter your name here:\n").strip().lower()
-
+    name = input("Unless your name is Deadpool.. Enter your name here:\n").strip().lower()
     if name == "":
         print("Any name will do..")
         game_intro()
